79343aa3e53b9979d5966548e18043015ebb516b
MaltheSigaard <au704390@post.au.dk>
2024-10-18 10:08:27 +0200

    Initial commit

---
+++ src/exercise-0x10/pangram.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+// Exercise 0x00 - include "pangram.h" here
+
+int main(int argc, char** argv) {
+
+// Test strings
+std::string case_0 = "A quick brown fox jumps over the lazy dog.";
+std::string case_1 = "When zombies arrive quickly, fax judge Pat.";
+std::string case_2 = "The five boxing wizards jump quickly.";
+std::string case_3 = "Amazingly few discotheques provide jukeboxes.";
+
+  // Exercise 0x00 - Setup a test for your implementation
+  std::cout << "TEST - Exercise 0x00" << std::endl; 
+
+  // Exercise 0x01 - Setup a test for your implementation
+  std::cout << "TEST - Exercise 0x01" << std::endl;
+
+  return 0;
+}
+
+// Exercise 0x00 - Implement the function from "pangram.h"
+
+// Exercise 0x01 - Implement the function from "program.h"
